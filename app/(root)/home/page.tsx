555eda99d611bc9ec81162a0793060ac1518761a
codebydolapo <bashorun115@gmail.com>
2025-05-21 12:35:07 +0100

    fix: minor UI fixes in editor

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -27,7 +27,7 @@ const MyLottieComponent = () => {
 
   return (
     <>
-      <div className="w-[25rem] h-[25rem] flex items-center justify-center rounded-full relative">
+      <div className="md:w-[25rem] md:h-[25rem] w-[98vw] h-[10rem] flex items-center justify-center rounded-full relative">
         <div className="w-full z-10" ref={animationRef!}></div>
         <Image alt="" src="/vector.svg" width={0} height={0} className='w-full h-full absolute self-center tint-gray-300' />
       </div>
@@ -40,11 +40,11 @@ export default function HomePage() {
 
 
   return (
-    <main className="flex space-x-2 items-center justify-start pt-20 flex-col no-scrollbar">
+    <main className="h-full flex space-x-2 items-center md:justify-start justify-center md:pt-20 flex-col no-scrollbar ">
       <MyLottieComponent />
-      <div className="flex flex-col items-center justify-around text-center space-y-3">
-        <p className="font-extrabold text-3xl text-[#1da1f2] ">Hello there!</p>
-        <p className="font-medium text-[#000000be]">Check the sidebar to begin creating or collaborating...</p>
+      <div className="flex flex-col items-center justify-around text-center space-y-3 px-2">
+        <p className="font-extrabold md:text-3xl text-xl text-[#1da1f2] ">Hello there!</p>
+        <p className="font-medium text-[#000000be] md:text-base text-sm">Check the sidebar to begin creating or collaborating...</p>
       </div>
     </main>
   );
